86ef76b51a1df53ec83d6f9571542751a72f44cb
Diegoarend <diegoaw@gmail.com>
2020-08-01 15:37:09 -0300

    Revert "create integration test - state GREEN"
    This reverts commit 3d826a6c33415b556b8260e9240a10c60fb14d1c.
    
    revert last commit

--- test/integration/helpers.js
+++
@@ -1,9 +0,0 @@
-// configurações dos testes
-// definimos as globais que vão poder ser acessadas sem a necessidade de fazer uma importação
-import supertest from 'supertest';
-import chai from 'chai';
-import app from '../../src/app';
-
-global.app = app;
-global.request = supertest(app);
-global.expect = chai.expect;
